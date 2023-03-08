73a2945e663bf85c2d0756b03273f87a59ace7dd
patriciasouzas <patriciasouzaa89@gmail.com>
2023-03-08 17:15:43 -0300

    adjustments in account

--- digital-wallet/src/main/java/com/matera/digitalwallet/model/entities/Account.java
+++ digital-wallet/src/main/java/com/matera/digitalwallet/model/entities/Account.java
@@ -1,4 +1,19 @@
 package com.matera.digitalwallet.model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
+    private int agency;
+    private int number;
+    private BigDecimal balance;
+    private Holder holder;
 }
