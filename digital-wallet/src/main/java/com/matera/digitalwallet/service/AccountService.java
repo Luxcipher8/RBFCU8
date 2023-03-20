5a5b3143fe82aa1a9cdc9fddbec2a0ee9faa8fce
patriciasouzas <patriciasouzaa89@gmail.com>
2023-03-20 13:08:45 -0300

    adjustments in services

--- digital-wallet/src/main/java/com/matera/digitalwallet/service/AccountService.java
+++ digital-wallet/src/main/java/com/matera/digitalwallet/service/AccountService.java
@@ -2,10 +2,15 @@ package com.matera.digitalwallet.service;
 
 import com.matera.digitalwallet.exceptions.ExistingAccountException;
 import com.matera.digitalwallet.exceptions.InvalidAccountException;
+import com.matera.digitalwallet.exceptions.InvalidBankException;
+import com.matera.digitalwallet.exceptions.InvalidOperationException;
+import com.matera.digitalwallet.model.dto.PixBacenDto;
 import com.matera.digitalwallet.model.dto.RequestAccountDto;
 import com.matera.digitalwallet.model.entities.Account;
+import com.matera.digitalwallet.model.entities.Bank;
 import com.matera.digitalwallet.model.entities.Holder;
 import com.matera.digitalwallet.repository.AccountRepository;
+import com.matera.digitalwallet.repository.BankRepository;
 import com.matera.digitalwallet.repository.HolderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,11 +23,17 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class AccountService {
-
     private final AccountRepository accountRepository;
     private final HolderRepository holderRepository;
+    private final BankRepository bankRepository;
+    private final PixBacenService pixBacenService;
 
     public Account createAccount(RequestAccountDto requestAccountDto) {
+
+        int code = requestAccountDto.getCode();
+        final Bank bank = bankRepository.findByBankCode(code).orElseThrow(
+                () -> new InvalidBankException("Banco não encontrado: " + code)
+        );
 
         final Holder holder = new Holder();
         holder.setCpf(requestAccountDto.getCpf());
@@ -32,9 +43,14 @@ public class AccountService {
         var account = new Account();
         account.setAgency(requestAccountDto.getAgency());
         account.setHolder(holder);
+        account.setBank(bank);
+        account.setPix(requestAccountDto.getPixKey());
         accountIsValid(account);
 
-        return accountRepository.save(account);
+        Account savedAccount = accountRepository.save(account);
+        pixBacenService.registerPixCentralBank(savedAccount.toBacenDto());
+
+        return savedAccount;
     }
 
     private void accountIsValid(Account account) {
@@ -76,6 +92,7 @@ public class AccountService {
     public void transfer(Long idAccountDebited, Long idAccountCredited, BigDecimal value) {
         Account accountDebited = findAccount(idAccountDebited);
         Account accountCredited = findAccount(idAccountCredited);
+
         accountDebited.debit(value);
         accountCredited.credit(value);
         List<Account> accounts = new ArrayList<>();
@@ -83,5 +100,29 @@ public class AccountService {
         accounts.add(accountDebited);
 
         accountRepository.saveAll(accounts);
+    }
+
+    public static void validateTransfer(Account debitedAccount, Account creditedAccount) {
+        if (debitedAccount.getBank().getBankCode() != creditedAccount.getBank().getBankCode()) {
+            throw new InvalidOperationException();
+        }
+    }
+
+    public void pix(Long debitedAccountId, String pixKey, BigDecimal value) {
+        final Account debitedAccount = findAccount(debitedAccountId);
+
+        PixBacenDto pixBacenDto = pixBacenService.findAccountCentralBank(pixKey);
+
+        Account creditedAccount = accountRepository.findByAgencyAndNumber(pixBacenDto.getAgency(), pixBacenDto.getNumber())
+                .orElseThrow(() -> new InvalidAccountException("Conta não encontrada: " + pixKey));
+
+        debitedAccount.debit(value);
+        creditedAccount.credit(value);
+
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(creditedAccount);
+        accounts.add(debitedAccount);
+        accountRepository.saveAll(accounts);
+
     }
 }
---
+++ digital-wallet/src/main/java/com/matera/digitalwallet/service/PixBacenService.java
@@ -0,0 +1,30 @@
+package com.matera.digitalwallet.service;
+
+import com.matera.digitalwallet.model.dto.PixBacenDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
+public class PixBacenService {
+    private static final String ACCOUNTS_BACEN_URL = "http://localhost:8088/contas";
+    private final static RestTemplate restTamplate = new RestTemplate();
+
+    public void registerPixCentralBank(PixBacenDto pixBacenDTO) {
+        HttpEntity<PixBacenDto> request = new HttpEntity<>(pixBacenDTO);
+        PixBacenDto pixBacenDto = restTamplate.postForObject(
+                ACCOUNTS_BACEN_URL,
+                request,
+                PixBacenDto.class);
+        log.info("Conta cadastrada com sucesso no Banco Central: {}", pixBacenDto);
+    }
+
+    public PixBacenDto findAccountCentralBank(String pixKey) {
+        String url = String.format("%s%s", ACCOUNTS_BACEN_URL, pixKey);
+        PixBacenDto pixBacenDto = restTamplate.getForObject(url, PixBacenDto.class);
+        log.info("Conta encontrada com sucesso no Banco Central: {}", pixBacenDto);
+        return pixBacenDto;
+    }
+}
