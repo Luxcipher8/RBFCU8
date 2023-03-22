c009e7dd535f816c83877ea00e6e93992ed0d59a
patriciasouzas <patriciasouzaa89@gmail.com>
2023-03-21 21:46:00 -0300

    added creation bank

--- digital-wallet/src/main/java/com/matera/digitalwallet/DigitalWalletApplication.java
+++ digital-wallet/src/main/java/com/matera/digitalwallet/DigitalWalletApplication.java
@@ -1,13 +1,27 @@
 package com.matera.digitalwallet;
 
+import com.matera.digitalwallet.model.entities.Bank;
+import com.matera.digitalwallet.repository.BankRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DigitalWalletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DigitalWalletApplication.class, args);
+		SpringApplication.run(DigitalWalletApplication.class);
+	}
+
+	@Autowired
+	private BankRepository repository;
+	@PostConstruct
+	public void createBank(){
+		Bank bank = new Bank();
+		bank.setBankName("NewBank");
+		repository.save(bank);
 	}
 
 }
